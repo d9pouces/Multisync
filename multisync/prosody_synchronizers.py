d9855f7ae7838a83e85eec0230390db55e2a46e1
mgallet <mgallet@19pouces.net>
2016-06-17 10:51:47 +0200

    allow  Django-side filtering

--- multisync/prosody_synchronizers.py
+++ multisync/prosody_synchronizers.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-from ConfigParser import ConfigParser
 import os
+from ConfigParser import ConfigParser
 
 from django.conf import settings
 
-from multisync.models import LdapUser, LdapGroup
+from multisync.ldap_synchronizers import LdapUserSynchronizer, LdapGroupSynchronizer
 from multisync.nrpe import NrpeCheck
-
 
 __author__ = 'Matthieu Gallet'
 
@@ -14,9 +13,11 @@ __author__ = 'Matthieu Gallet'
 class ProsodySynchronizer(NrpeCheck):
 
     def synchronize(self):
-        users = {x.name: x.display_name for x in LdapUser.objects.all()}
+        user_synchronizer = LdapUserSynchronizer()
+        group_synchronizer = LdapGroupSynchronizer()
+        users = {x.name: x.display_name for x in user_synchronizer.get_ref_elements()}
         parser = ConfigParser()
-        for group in LdapGroup.objects.all():
+        for group in group_synchronizer.get_ref_elements():
             group_name = group.name.encode('utf-8')
             parser.add_section(group_name)
             for username in group.members:
